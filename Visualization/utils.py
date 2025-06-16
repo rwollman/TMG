30da804cd3a54dedb2d4ffa47b54edb7877ccbf7
Roy Wollman <rwollman@ucla.edu>
2025-06-16 15:46:12 -0700

    Remove empty utils.py file - no longer needed after consolidation

--- Visualization/utils.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-import math
-
-# Note: scale_vec() has been moved to TMG.Utils.coloru
-# All other functions in this file were unused and have been removed during cleanup.
-# This file is maintained for potential future utilities.
